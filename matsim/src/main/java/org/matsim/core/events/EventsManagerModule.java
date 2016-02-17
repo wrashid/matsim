83ad40c68abc7bc25ffb52fd586d37528830ccb2
Michael Zilske <michael.zilske@tu-berlin.de>
2016-02-17 16:21:17 +0100

    Use the unified ParallelEventsManager for everything.

--- matsim/src/main/java/org/matsim/core/events/EventsManagerModule.java
+++ matsim/src/main/java/org/matsim/core/events/EventsManagerModule.java
@@ -11,17 +11,7 @@ public class EventsManagerModule extends AbstractModule {
 
 	@Override
 	public void install() {
-		if (getConfig().parallelEventHandling().getOneThreadPerHandler() != null && getConfig().parallelEventHandling().getOneThreadPerHandler()) {
-			bindEventsManager().to(ParallelEventsManager.class).asEagerSingleton();
-		} else if (getConfig().parallelEventHandling().getNumberOfThreads() != null) {
-			if (getConfig().parallelEventHandling().getSynchronizeOnSimSteps() != null && getConfig().parallelEventHandling().getSynchronizeOnSimSteps()) {
-				bindEventsManager().to(SimStepParallelEventsManagerImpl.class).asEagerSingleton();
-			} else {
-				bindEventsManager().to(ParallelEventsManagerImpl.class).asEagerSingleton();
-			}
-		} else {
-			bindEventsManager().to(SimStepParallelEventsManagerImpl.class).asEagerSingleton();
-		}
+		bindEventsManager().to(ParallelEventsManager.class).asEagerSingleton();
 		bind(EventHandlerRegistrator.class).asEagerSingleton();
 	}
 
