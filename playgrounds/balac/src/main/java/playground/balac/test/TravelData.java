87f3f371876b4cbdeccf37c42ead10564331f334
balacmi <milos.balac@ivt.baug.ethz.ch>
2017-08-14 16:04:31 +0200

    analysis for scoring

---
+++ playgrounds/balac/src/main/java/playground/balac/test/TravelData.java
@@ -0,0 +1,16 @@
+package playground.balac.test;
+
+public class TravelData {
+	
+	String id;
+	double startTime = 0.0;
+	double endTime = 0.0;
+	String mode;
+	
+	
+	public String toString() {
+		return id +  ";" + startTime +  ";" + endTime +  ";" + mode;		
+		
+	}
+
+}
