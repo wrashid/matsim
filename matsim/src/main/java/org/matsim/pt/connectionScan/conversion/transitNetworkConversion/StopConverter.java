006e7ca8e4a143f3ba3887c05d7f1ba7ead4e66f
gabrielthunig <gabriel.thunig@web.de>
2017-11-17 16:54:16 +0100

    route with interchange

--- matsim/src/main/java/org/matsim/pt/connectionScan/conversion/transitNetworkConversion/StopConverter.java
+++ matsim/src/main/java/org/matsim/pt/connectionScan/conversion/transitNetworkConversion/StopConverter.java
@@ -6,10 +6,15 @@ import edu.kit.ifv.mobitopp.publictransport.model.Station;
 import edu.kit.ifv.mobitopp.publictransport.model.Stop;
 import org.matsim.api.core.v01.Coord;
 import org.matsim.api.core.v01.Id;
+import org.matsim.core.utils.collections.QuadTree;
+import org.matsim.pt.connectionScan.utils.CoordinateUtils;
+import org.matsim.pt.router.TransitTravelDisutility;
 import org.matsim.pt.transitSchedule.api.TransitStopFacility;
 
 import java.awt.geom.Point2D;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
@@ -23,15 +28,22 @@ class StopConverter {
     private Map<Id<TransitStopFacility>, TransitStopFacility> matsimStops;
     private List<Stop> connectionScanStops = new ArrayList<>();
     private MappingHandler idAndMappingHandler;
+    private final double maxBeelineWalkConnectionDistance;
+    private TransitTravelDisutility costFunction;
+    private QuadTree<Stop> qtStops = null;
 
     /**
      * TODO
      * @param idAndMappingHandler
+     * @param costFunction
      */
-    StopConverter(Map<Id<TransitStopFacility>, TransitStopFacility> matsimStops, MappingHandler idAndMappingHandler) {
+    StopConverter(Map<Id<TransitStopFacility>, TransitStopFacility> matsimStops, MappingHandler idAndMappingHandler,
+                  double maxBeelineWalkConnectionDistance, TransitTravelDisutility costFunction) {
 
         this.matsimStops = matsimStops;
         this.idAndMappingHandler = idAndMappingHandler;
+        this.maxBeelineWalkConnectionDistance = maxBeelineWalkConnectionDistance;
+        this.costFunction = costFunction;
     }
 
     /**
@@ -44,8 +56,63 @@ class StopConverter {
         for (TransitStopFacility matsimStop : matsimStops.values()) {
             convertAndAddStop(matsimStop);
         }
+        addNeighbours();
     }
 
+    public void buildQuadTree() {
+        double minX = Double.POSITIVE_INFINITY;
+        double minY = Double.POSITIVE_INFINITY;
+        double maxX = Double.NEGATIVE_INFINITY;
+        double maxY = Double.NEGATIVE_INFINITY;
+
+        for (Stop stop : this.connectionScanStops) {
+            Coord c = CoordinateUtils.convert2Coord(stop.coordinate());
+            if (c.getX() < minX) {
+                minX = c.getX();
+            }
+            if (c.getY() < minY) {
+                minY = c.getY();
+            }
+            if (c.getX() > maxX) {
+                maxX = c.getX();
+            }
+            if (c.getY() > maxY) {
+                maxY = c.getY();
+            }
+        }
+
+        QuadTree<Stop> quadTree = new QuadTree<>(minX, minY, maxX, maxY);
+        for (Stop stop : this.connectionScanStops) {
+            Coord c = CoordinateUtils.convert2Coord(stop.coordinate());
+            quadTree.put(c.getX(), c.getY(), stop);
+        }
+        this.qtStops = quadTree;
+    }
+
+    private void addNeighbours() {
+
+        buildQuadTree();
+        for (Stop stop : connectionScanStops) {
+            for (Stop nearStop : getNearestNodes(stop.coordinate(), maxBeelineWalkConnectionDistance)) {
+                if ((stop != nearStop)) {
+                    if (stop.station() != nearStop.station()) {
+                        stop.addNeighbour(nearStop, calcWalkTime(stop, nearStop));
+                    }
+                }
+            }
+        }
+    }
+
+    private RelativeTime calcWalkTime(Stop stop1, Stop stop2) {
+        double walkTime = costFunction.getWalkTravelTime(null,
+                CoordinateUtils.convert2Coord(stop1.coordinate()),
+                CoordinateUtils.convert2Coord(stop2.coordinate()));
+        return RelativeTime.of((long)walkTime, ChronoUnit.SECONDS);
+    }
+
+    public Collection<Stop> getNearestNodes(final Point2D coord, final double distance) {
+        return this.qtStops.getDisk(coord.getX(), coord.getY(), distance);
+    }
 
     private void convertAndAddStop(TransitStopFacility matsimStop) {
         Stop stop = convertStop(matsimStop);
@@ -61,7 +128,7 @@ class StopConverter {
     private Stop convertStop(TransitStopFacility matsimStop) {
         int id = idAndMappingHandler.createAndMapId(matsimStop);
         String name = matsimStop.getName();
-        Point2D location = convert2Point2D(matsimStop.getCoord());
+        Point2D location = CoordinateUtils.convert2Point2D(matsimStop.getCoord());
         RelativeTime changeTime = RelativeTime.ZERO;
         Station station = convert2Station(id);
         Stop stop = new Stop(id, name, location, changeTime, station, 0);
@@ -70,19 +137,9 @@ class StopConverter {
     }
 
     /**
-     * Converts a matsim/Coord into a Point2D.
-     *
-     * @param matsimCoord the matsim/TransitStopFacility that contains the required matsim/Coord
-     * @return the converted Point2D
-     */
-    private Point2D convert2Point2D(Coord matsimCoord) {
-        return new Point2D.Double(matsimCoord.getX(), matsimCoord.getY());
-    }
-
-    /**
      * Converts a matsim/TransitStopFacility into a connection-scan/Station.
      *
-     * @param matsimStop the matsim/TransitStopFacility that contains the required matsim/Id
+     * @param id the matsim/TransitStopFacility ID that contains the required matsim/Id
      * @return the converted connection-scan Station
      */
     private Station convert2Station(int id) {
