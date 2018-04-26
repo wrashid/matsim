c44045db7ce0381073b56e320d3dc8236b68e15d
Johan W Joubert <johan.joubert@up.ac.za>
2018-04-26 17:13:38 +0200

    Added comments for future consideration

--- contribs/freight/src/main/java/org/matsim/contrib/freight/usecases/receiver/MyReceiverOrderStrategyManagerFactorImpl.java
+++ contribs/freight/src/main/java/org/matsim/contrib/freight/usecases/receiver/MyReceiverOrderStrategyManagerFactorImpl.java
@@ -29,6 +29,13 @@ public class MyReceiverOrderStrategyManagerFactorImpl implements ReceiverOrderSt
 			stratMan.addStrategy(strategy, null, 1.0);
 
 		}
+		
+		/*TODO:
+		 * 1. Consider a "time window wiggle" replanning strategy that searches 
+		 *    randomly within the current time window, but also between different
+		 *    time windows if there are multiple.
+		 * 2. Consider/Add a ConfigModule.
+		 */
 
 		return stratMan;
 	}
