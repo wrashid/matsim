ce7b09f0fdf13ea51143b4e003a7ed699ba044a2
gabrielthunig <gabriel.thunig@web.de>
2018-06-21 10:20:17 +0200

    differentiate only between connection-scan and standard also list expected errors and reason them

--- matsim/src/test/java/org/matsim/pt/router/ConnectionScanRouterTest.java
+++ matsim/src/test/java/org/matsim/pt/router/ConnectionScanRouterTest.java
@@ -38,9 +38,13 @@ public class ConnectionScanRouterTest extends TransitRouterImplTest {
     public static Collection<Object> createRouterTypes() {
         Object[] router = new Object[]{
 //                "standard",
-//                "raptor",
-//                "connectionScan",
-//                "otp"
+                "connectionScan"
+                // expect 4 tests to fail.
+                // 2 because of lineSwitchUtility (connection scan does not support that)
+                // 1 because of travel-time-differences in nanoSeconds-range (CS does jet not support that either)
+                // 1 because in CS you cannot enter a vehicle the same second you start at a stop
+                // further issues that don't lead to failed tests here:
+                //
         };
         return Arrays.asList(router);
     }
