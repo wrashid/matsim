e9f0d68b5f234191f4bff77ee5c760b66246844e
Amit Agarwal <agarwal@vsp.tu-berlin.de>
2018-07-19 07:50:42 +0200

    also moving some link analysis codes

--- contribs/emissions/src/main/java/org/matsim/contrib/emissions/spatialAnalysis/AnalysisExample.java
+++ contribs/emissions/src/main/java/org/matsim/contrib/emissions/spatialAnalysis/AnalysisExample.java
@@ -19,6 +19,14 @@
 
 package org.matsim.contrib.emissions.spatialAnalysis;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import org.matsim.api.core.v01.Id;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.network.Link;
+import org.matsim.contrib.emissions.types.WarmPollutant;
+
 /**
  * Created by Amit on 7/18/2018.
  */
@@ -27,7 +35,71 @@ public class AnalysisExample {
 
     public static void main(String[] args) {
 
+        Map<Double,Map<Id<Link>,SortedMap<String,Double>>> linkEmissionsBau = new HashMap<>();
+        Map<Double,Map<Id<Link>,SortedMap<String,Double>>> linkEmissionsPolicy = new HashMap<>();
 
+        // setting of input data
+        SpatialDataInputs inputs = new SpatialDataInputs(SpatialDataInputs.EmissionSourceMethod.line,bau, policyCase);
+        inputs.setBoundingBox(xMin, xMax, yMin, yMax);
+        inputs.setTargetCRS(targetCRS);
+        inputs.setGridInfo(GridType.HEX, gridSize);
+        inputs.setSmoothingRadius(smoothingRadius);
+        inputs.setShapeFile(shapeFile);
+
+//		SpatialInterpolation plot = new SpatialInterpolation(inputs,runDir+"/analysis/spatialPlots/"+noOfBins+"timeBins/");
+        SpatialInterpolation plot = new SpatialInterpolation(inputs,runDir+"/analysis/spatialPlots/"+noOfBins+"timeBins/", true);
+
+        EmissionLinkAnalyzer emsLnkAna = new EmissionLinkAnalyzer(LoadMyScenarios.getSimulationEndTime(inputs.initialCaseConfig), inputs.initialCaseEmissionEventsFile, noOfBins);
+        emsLnkAna.preProcessData();
+        emsLnkAna.postProcessData();
+        linkEmissionsBau = emsLnkAna.getLink2TotalEmissions();
+
+        if(inputs.isComparing){
+            emsLnkAna = new EmissionLinkAnalyzer(LoadMyScenarios.getSimulationEndTime(inputs.compareToCaseConfig), inputs.compareToCaseEmissionEventsFile, noOfBins);
+            emsLnkAna.preProcessData();
+            emsLnkAna.postProcessData();
+            linkEmissionsPolicy = emsLnkAna.getLink2TotalEmissions();
+        }
+
+        Scenario sc = LoadMyScenarios.loadScenarioFromNetwork(inputs.initialCaseNetworkFile);
+
+        EmissionTimebinDataWriter writer = new EmissionTimebinDataWriter();
+        writer.openWriter(runDir+"/analysis/spatialPlots/"+noOfBins+"timeBins/"+"viaData_NO2_"+GridType.HEX+"_"+gridSize+"_"+smoothingRadius+"_line_"+policyName+"_diff.txt");
+
+        for(double time :linkEmissionsBau.keySet()){
+            for(Link l : sc.getNetwork().getLinks().values()){
+                Id<Link> id = l.getId();
+
+                if(plot.isInResearchArea(l)){
+                    double emiss = 0;
+                    if(inputs.isComparing){
+                        double linkEmissionBau =0;
+                        double linkEmissionPolicy =0;
+
+                        if(linkEmissionsBau.get(time).containsKey(id) && linkEmissionsPolicy.get(time).containsKey(id)) {
+                            linkEmissionBau = countScaleFactor * linkEmissionsBau.get(time).get(id).get(WarmPollutant.NO2.toString());
+                            linkEmissionPolicy = countScaleFactor * linkEmissionsPolicy.get(time).get(id).get(WarmPollutant.NO2.toString());
+                        } else if(linkEmissionsBau.get(time).containsKey(id)){
+                            linkEmissionBau = countScaleFactor * linkEmissionsBau.get(time).get(id).get(WarmPollutant.NO2.toString());
+                        } else if(linkEmissionsPolicy.get(time).containsKey(id)){
+                            linkEmissionPolicy = countScaleFactor * linkEmissionsPolicy.get(time).get(id).get(WarmPollutant.NO2.toString());
+                        }
+                        emiss = linkEmissionPolicy - linkEmissionBau;
+
+                    } else {
+                        if(linkEmissionsBau.get(time).containsKey(id)) emiss = countScaleFactor * linkEmissionsBau.get(time).get(id).get(WarmPollutant.NO2.toString());
+                        else emiss =0;
+                    }
+
+                    plot.processLink(l,  emiss);
+
+                }
+            }
+            writer.writeData(time, plot.getCellWeights());
+            //			plot.writeRData("NO2_"+(int)time/3600+"h",isWritingGGPLOTData);
+            plot.reset();
+        }
+        writer.closeWriter();
 
 
     }
