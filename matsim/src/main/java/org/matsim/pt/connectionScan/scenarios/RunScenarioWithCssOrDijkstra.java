e16be443badb35ddbac5f20c4f54ace3aa0173c5
gabrielthunig <gabriel.thunig@web.de>
2018-10-04 10:55:59 +0200

    current run status

--- matsim/src/main/java/org/matsim/pt/connectionScan/scenarios/RunScenarioWithCssOrDijkstra.java
+++ matsim/src/main/java/org/matsim/pt/connectionScan/scenarios/RunScenarioWithCssOrDijkstra.java
@@ -19,20 +19,28 @@ public class RunScenarioWithCssOrDijkstra {
         File file = new File("");
         System.out.println(file.getAbsolutePath());
 
-        String configPath = "../..\\runs-svn\\open_berlin_scenario\\b5_22e1a\\b5_22e1a.run_config.xml";
+//        String configPath = "C:\\Users\\gthunig\\VSP\\matsim\\matsim\\matsim-berlin\\scenarios\\berlin-v5.1-1pct\\b5_1\\b5_1.run_config.xml";
+        String configPath = "C:\\Users\\gthunig\\Desktop\\Vsp\\ConnectionScan\\Scenarien\\Cottbus\\cb04\\run_config.xml";
         boolean useCss = true;
 //        String configPath = args[0];
 //        boolean useCss = Boolean.parseBoolean(args[1]);
 
 
         Config config = ConfigUtils.loadConfig(configPath);
-//        config.plans().setInputFile("./b5_22e1a.1pct_plans_pt300.xml.gz"); // <-currently default
-        config.plans().setInputFile("./b5_22e1a.1pct_plans_pt1.xml.gz");
+//        config.plans().setInputFile("./plansTest4.xml.gz");
+        config.global().setNumberOfThreads(1);
+        config.planCalcScore().setUtilityOfLineSwitch(0); //-- compareable
 
         Scenario scenario = ScenarioUtils.loadScenario(config);
 
-//        scenario.getConfig().controler().setLastIteration(0);
-        scenario.getConfig().controler().setOutputDirectory("./output/CSCompare2");
+        scenario.getConfig().controler().setLastIteration(0);
+        if (useCss) {
+            scenario.getConfig().controler().setOutputDirectory("./output/CottbusScenario/CSTest");
+//            scenario.getConfig().controler().setOutputDirectory("./output/BerlinScenario/CS2");
+        } else {
+//            scenario.getConfig().controler().setOutputDirectory("./output/CottbusScenario/DTest");
+            scenario.getConfig().controler().setOutputDirectory("./output/CottbusScenario/D");
+        }
         scenario.getConfig().controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
 
         Controler controler = new Controler(scenario);
